1c5ba87d5492b12ec2d6030ab14bbcd1e0b5cb81
Vitor Angelozi <vitorangelozi2013@gmail.com>
2026-09-13 20:46:15 -0400

    db

--- database/migrations/2026_09_10_004805_create_pokemon_table.php
+++ database/migrations/2026_09_10_004805_create_pokemon_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('pokemon', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->int('lv');
             $table->timestamps();
         });
     }
