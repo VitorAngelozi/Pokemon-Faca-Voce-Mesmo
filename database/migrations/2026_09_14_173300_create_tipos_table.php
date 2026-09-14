59b332c2f8940d323baea05b76aa4b4a8cb73f54
Vitor Angelozi <vitorangelozi2013@gmail.com>
2026-09-14 14:36:20 -0300

    feat: treinador table and tables on pokemon

--- database/migrations/2026_09_14_173300_create_tipos_table.php
+++ database/migrations/2026_09_14_173300_create_tipos_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('tipos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->text('descricao');
             $table->timestamps();
         });
     }
