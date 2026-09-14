78f646be57b446d39a343ca9c0be0eca3881f75f
Vitor Angelozi <vitorangelozi2013@gmail.com>
2026-09-13 20:49:28 -0400

    migration rename

--- database/migrations/2026_09_10_004805_create_pokemon_table.php
+++ database/migrations/2026_09_10_004805_create_pokemon_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('pokemon', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
-            $table->int('lv');
+            $table->string('tipo');
             $table->timestamps();
         });
     }
